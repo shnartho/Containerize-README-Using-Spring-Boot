1d103c61db5aa4dab1b65f4ec8902d17a85e869e
shnartho <shnartho@gmail.com>
2022-05-16 02:31:43 +0200

    containerize

---
+++ src/test/java/com/nayem/bootingweb/BootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayem.bootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
